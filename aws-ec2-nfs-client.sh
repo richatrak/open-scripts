64b6b87d5bb69a6cf29c2de3bce4fe0959852228
Rich <rich1983@users.noreply.github.com>
2018-11-19 10:14:20 +0800

    Update aws-ec2-nfs-client.sh

--- aws-ec2-nfs-client.sh
+++ aws-ec2-nfs-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 mkdir -p /data/nfs
-echo NFS-Server:/data/nfs /data/nfs nfs nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2,noresvport 0 0 >> /etc/fstab
+echo NFS-Server:/data/nfs /data/nfs nfs defaults,nofail 0 2 >> /etc/fstab
 mount -a
