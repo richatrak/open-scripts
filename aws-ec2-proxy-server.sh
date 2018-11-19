9a7a46637ffae9f633b6ecef93c4f224052bc553
Rich <rich1983@users.noreply.github.com>
2018-11-19 10:03:02 +0800

    Create aws-ec2-proxy-server.sh

---
+++ aws-ec2-proxy-server.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# ----- Variable -----
+confStartLine=$(grep -n "    server {" nginx.conf | head -n 1 | awk -F ":" '{print $1}')
+confEndLine=$(grep -n "^    }" nginx.conf | head -n 1 | awk -F ":" '{print $1}')
+
+varHostName=$(cat /etc/aws.ec2.instance.name)
+varProxyTarget=${varHostName:0:-5}
+
+# ----- Install -----
+amazon-linux-extras install nginx1.12 -y
+
+# ----- Pre-Config -----
+sed -i -e "${confStartLine},${confEndLine}s/^/#/g" /etc/nginx/nginx.conf
+sed -i "${confStartLine} i \ \ \ \ include /etc/nginx/sites-enabled/*.conf" nginx.conf
+
+mkdir -p /etc/nginx/sites-available
+mkdir -p /etc/nginx/sites-enabled
+
+# ----- Site Config -----
+cat << EOF >> /etc/nginx/sites-available/${varHostName}.localdomain.conf
+server {
+  listen 80 default server;
+  listen [::]:80 default server;
+  server_name _;
+  root /usr/share/nginx/html;
+  
+  include /etc/nginx/default.d/*.conf;
+
+  location / {
+     proxy_pass http://${varProxyTarget};
+  }
+}
+
+upstream ${varProxyTarget} {
+}
+EOF
+
+ln -s /etc/sites-available/${varHostName}.localdomain.conf /etc/sites-enabled/${varHostName}.localdomain.conf
