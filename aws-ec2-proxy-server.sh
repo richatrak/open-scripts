c354aa8748704fb288cf8c7b8ff092b77ce9edb7
Rich <rich1983@users.noreply.github.com>
2018-12-12 19:35:15 +0800

    Update aws-ec2-proxy-server.sh

--- aws-ec2-proxy-server.sh
+++ aws-ec2-proxy-server.sh
@@ -31,10 +31,10 @@ server {
 
   location / {
      proxy_pass http://${varProxyTarget};
-     proxy_set_header Host $host:$server_port;
-     proxy_set_header X-Real-IP $remote_addr;
-     proxy_set_header X-Real-PORT $remote_port;
-     proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+     proxy_set_header Host \$host:\$server_port;
+     proxy_set_header X-Real-IP \$remote_addr;
+     proxy_set_header X-Real-PORT \$remote_port;
+     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
   }
 }
 
