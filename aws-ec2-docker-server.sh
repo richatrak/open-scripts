e69e49a55950f5f9d3913c52f05d0a8a12992125
Rich <rich1983@users.noreply.github.com>
2020-03-16 13:57:40 +0800

    Remove support from Zeus Server

--- aws-ec2-docker-server.sh
+++ aws-ec2-docker-server.sh
@@ -3,13 +3,7 @@
 SERVER_NAME=$(cat /etc/aws.ec2.instance.name)
 DOCKER_IMAGE_FULL_NAME=280666639932.dkr.ecr.us-west-2.amazonaws.com/iadea.service.$((cat /etc/aws.ec2.instance.name) | tr '[:upper:]' '[:lower:]' | awk 'BEGIN {FS="_"}; {print $1}').v1:latest
 
-REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
-REGION=${REGION:0:-1}
-
-source <(curl -s https://zeus.1adea.com/api/iCare.safeServer.awsKey?region=${REGION})
-
 amazon-linux-extras install docker
-# yum install -y docker
 
 service docker start
 systemctl enable docker
